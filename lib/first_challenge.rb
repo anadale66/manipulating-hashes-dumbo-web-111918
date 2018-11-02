53d5015ee3335f597718381be4a46e1797ed7f62
Roman Pirag <mr.anadale@yahoo.com>
2018-11-02 20:56:24 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -25,7 +25,7 @@ def first_challenge
   end
   
   
-  #contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
 
 
   #remember to return your newly altered contacts hash!
